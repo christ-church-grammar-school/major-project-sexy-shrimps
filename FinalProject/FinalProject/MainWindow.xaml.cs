a5c4536830ea797ba06d6b5bb73b0fad7ff8024f
Salty-Salter <65321289+Salty-Salter@users.noreply.github.com>
2020-08-09 15:14:56 +0800

    started design of main window - submission test

--- FinalProject/FinalProject/MainWindow.xaml.cs
+++ FinalProject/FinalProject/MainWindow.xaml.cs
@@ -44,5 +44,10 @@ namespace FinalProject
         {
             MainFrame.Navigate(new Uri("Page1.xaml", UriKind.Relative));
         }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
     }
 }
