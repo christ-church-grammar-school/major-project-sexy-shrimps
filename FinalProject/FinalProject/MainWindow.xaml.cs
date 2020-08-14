dd7cf7743f98d0d428d42c04560662c1f8c0d91c
Salty-Salter <65321289+Salty-Salter@users.noreply.github.com>
2020-08-14 17:05:34 +0800

    checks if student number file exists

--- FinalProject/FinalProject/MainWindow.xaml.cs
+++ FinalProject/FinalProject/MainWindow.xaml.cs
@@ -65,7 +65,11 @@ namespace FinalProject
                 tw.Close();
             }
 
-            MainFrame.Navigate(new Uri("Hub.xaml", UriKind.Relative));
+            string login = "../../Users/" + student.Text + ".txt";
+            if (File.Exists(login))
+            {
+                MainFrame.Navigate(new Uri("Hub.xaml", UriKind.Relative));
+            }
         }
 
         private void MainFrame_Navigated(object sender, NavigationEventArgs e)
