ae323fd804d603cf347a3e8f998f70c1aceccd4f
Callum Burnet <CalCal29@users.noreply.github.com>
2020-09-25 08:44:16 +0800

    Passwordpog

--- FinalProject/FinalProject/UsInfo.xaml.cs
+++ FinalProject/FinalProject/UsInfo.xaml.cs
@@ -34,12 +34,59 @@ namespace FinalProject
         private void save_clicked(object sender, RoutedEventArgs e)
         {
             string login = "../../Users/" + student_id.Text;
+            string student_path = "../../Users/" + student_id.Text + "/student.txt";
+            string studentpath_color = "../../Users/" + student_id.Text + "/color.txt";
             if (Directory.Exists(login))
             {
                 this.Close();
                 return;
             }
             DirectoryInfo di = Directory.CreateDirectory(login);
+            if (!File.Exists(student_path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(student_path))
+                {
+                    sw.WriteLine(student_id.Text);
+                    sw.WriteLine(student_password.Text);
+                    sw.WriteLine("");
+                    sw.WriteLine(student_name.Text);
+
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(student_path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            if (!File.Exists(studentpath_color))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(studentpath_color))
+                {
+                    sw.WriteLine("#ffe0cc"+Environment.NewLine+ "#ccffcc" + Environment.NewLine+ "#ffccf5"+ Environment.NewLine+ "#f5ffcc"+ Environment.NewLine+ "#ccffe0"+ Environment.NewLine +"#ffcce0"+ Environment.NewLine+ "#ccccff"+ Environment.NewLine+ "#ffe0cc"+ Environment.NewLine+ "#cce0ff"+ Environment.NewLine+ "#ffcce0"+ Environment.NewLine+ "#ccccff"+ Environment.NewLine+"#ccffcc"+ Environment.NewLine+ "#f5ffcc+"+Environment.NewLine+ "#ffccf5"+ Environment.NewLine+ "#ffe0cc"+ Environment.NewLine+ "#ffccf5"+ Environment.NewLine+ "#f5ffcc"+ Environment.NewLine+ "#ccffe0"+Environment.NewLine+ "#cce0ff"+ Environment.NewLine+ "#ccfff5"+ Environment.NewLine + "#ccffcc" + Environment.NewLine + "#ffe0cc" + Environment.NewLine + "#ffe0cc" + Environment.NewLine + "#ccccff" + Environment.NewLine + "f5ffcc" + Environment.NewLine + "#cce0ff" + Environment.NewLine + "#ccfff5" + Environment.NewLine + "ccffe0" + Environment.NewLine + "#ffe0cc" + Environment.NewLine + "#ccffcc" + Environment.NewLine + "#ffcce0" + Environment.NewLine + "#ccffe0" + Environment.NewLine + "#ccccff" + Environment.NewLine + "#e4e6e4" + Environment.NewLine + "#e4e6e4" +;
+                    ;
+
+
+
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(studentpath_color))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
 
 
 
