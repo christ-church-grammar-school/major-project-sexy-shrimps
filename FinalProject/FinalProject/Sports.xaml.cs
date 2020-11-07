f6a54f3b44e3eb1a55408467af7cb887e1038271
CallumBreen <1019628@student.ccgs.wa.edu.au>
2020-11-07 17:08:45 +0800

    Automatic updates
    Fixtures now automatically update based on the date
    
    Fixtures update every monday to show the game that weekend/friday
    
    Displays "CCGS Bye" on bye days

--- FinalProject/FinalProject/Sports.xaml.cs
+++ FinalProject/FinalProject/Sports.xaml.cs
@@ -39,11 +39,11 @@ namespace FinalProject
             Navigate();
         }
 
-        
+
 
         private void Navigate()
         {
-            
+
 
             string pathuser = "../../user.txt";
             string user = File.ReadLines(pathuser).First();
@@ -55,7 +55,7 @@ namespace FinalProject
             HtmlDocument doc = web.Load("http://sport.ccgs.wa.edu.au/Fixtures_Teams.asp?Id=28729");
 
             foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//a[@href]"))
-            {       
+            {
                 if (node.InnerText == sport)
                 {
                     string hrefValue = node.GetAttributeValue("href", string.Empty);
@@ -82,24 +82,65 @@ namespace FinalProject
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(link);
 
-            
 
-            HtmlNode[] date = doc.DocumentNode.SelectSingleNode("//tr[@id]")
-                 .SelectNodes(".//td[@style='white-space:nowrap; text-align:center']").ToArray();
-            HtmlNode[] venue = doc.DocumentNode.SelectSingleNode("//tr[@id]")
-                 .SelectNodes(".//a[@class='pu_MapLink']").ToArray();
-            HtmlNode[] opponent = doc.DocumentNode.SelectSingleNode("//tr[@id]")
-                 .SelectNodes(".//span[@class='FixtureListOpponent']").ToArray();
-            HtmlNode[] result = doc.DocumentNode.SelectSingleNode("//tr[@id]")
-                 .SelectNodes(".//td[@style='white-space:nowrap']").ToArray();
+
+            HtmlNode[] date = doc.DocumentNode.SelectNodes(".//td[@style='white-space:nowrap; text-align:center']").ToArray();
 
             List<string> date2 = date[0].InnerHtml.Split(new string[] { "<br>" }, StringSplitOptions.None).Select(s => HtmlNode.CreateNode($"<span>{s}</span>").InnerText).ToList();
-            List<string> result2 = result[0].InnerHtml.Split(new string[] { "<br>" }, StringSplitOptions.None).Select(s => HtmlNode.CreateNode($"<span>{s}</span>").InnerText).ToList();
+            int i;
+            for (i = 0; i < date.Length; i += 1)
+            {
+                
+                date2 = date[i].InnerHtml.Split(new string[] { "<br>" }, StringSplitOptions.None).Select(s => HtmlNode.CreateNode($"<span>{s}</span>").InnerText).ToList();
 
-            date_text.Text = $"{date2[0]}\n{date2[1]}\n{date2[2]}";
-            venue_text.Text = venue[0].GetAttributeValue("title", string.Empty);
-            opponent_text.Text = opponent[0].InnerText;
-            result_text.Text = $"{result2[0]}\n{result2[1]}";
+
+                List<string> date10 = date2[2].Split().ToList();
+
+                int monthIndex;
+                string desiredMonth = date10[1];
+                string[] MonthNames = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+                monthIndex = Array.IndexOf(MonthNames, desiredMonth) + 1;
+
+                DateTime date6 = Convert.ToDateTime($"{monthIndex.ToString()}/{date10[0].ToString()}/{date10[2].ToString()}");
+
+                DateTime datenow = DateTime.Today;///Convert.ToDateTime("01/01/2020")
+                TimeSpan span = new TimeSpan(2, 0, 0, 0, 0);
+                datenow -= span;
+
+                if (DateTime.Compare(datenow, date6) < 0)
+                {
+                    break;
+                }
+                
+
+            }
+            HtmlNode[] fixtures = doc.DocumentNode.SelectNodes(".//tr[@class='FixtureSummary']").ToArray();
+
+            if (i > fixtures.Length-1)
+            {
+                i = fixtures.Length - 1;
+            }
+            HtmlNode[] opponent = fixtures[i].SelectNodes(".//span[@class='FixtureListOpponent']").ToArray();
+            if (opponent[0].InnerText == "CCGS BYE")
+            {
+                date_text.Text = "";
+                venue_text.Text = "";
+                opponent_text.Text = "CCGS BYE";
+                result_text.Text = "";
+            }
+            else
+            {
+                HtmlNode[] venue = fixtures[i].SelectNodes(".//a[@class='pu_MapLink']").ToArray();
+                HtmlNode[] result = fixtures[i].SelectNodes(".//td[@style='white-space:nowrap']").ToArray();
+
+                List<string> result2 = result[0].InnerHtml.Split(new string[] { "<br>" }, StringSplitOptions.None).Select(s => HtmlNode.CreateNode($"<span>{s}</span>").InnerText).ToList();
+
+                date_text.Text = $"{date2[0]}\n{date2[1]}\n{date2[2]}";
+                venue_text.Text = venue[0].GetAttributeValue("title", string.Empty);
+                opponent_text.Text = opponent[0].InnerText;
+                result_text.Text = $"{result2[0]}\n{result2[1]}";
+            }
+            
         }
 
         private void Return_Clicked(object sender, RoutedEventArgs e)
